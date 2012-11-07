baa8b6832acb1192303c97714f108967d43bf37b
marcio <mgalli@telasocial.com>
2012-11-07 19:19:14 -0200

    tela eventos

---
+++ const.js
@@ -0,0 +1,11 @@
+
+URL_DESTAQUES = "/channel/destaques.xml";
+URL_TYPING = "/channel/destaques.xml";
+URL_GRADE ="/channel-store/grade";
+
+TEMPO_INICIO_MIDIA = 5000; 
+TEMPO_REFRESH_MIDIA = 80000; 
+TEMPO_REFRESH = 60000; 
+TEMPO_REFRESH_WEATHER = 90000; 
+
+
